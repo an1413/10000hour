c5d91ce5ce830c314741dd4d72c01bb0c1767d8c
an1413 <87430624+an1413@users.noreply.github.com>
2023-05-15 23:55:05 +0900

    add: 10000시간 react version

--- 10000hours/src/App.js
+++ 10000hours/src/App.js
@@ -1,7 +1,37 @@
+import { useState } from "react";
+import Footer from "./Components/footer/Footer";
+import Header from "./Components/header/Header";
+import Main from "./Components/main/Main";
+import Modal from "./Components/modal/Modal";
+
+
 function App() {
+  // 모달을 껐다 켰다 하려면 모달의 상태 저장  기본 값은 false! 모달 열려있지 않음
+  // const [modalShow, setModalShow] = useState(false);  
+
+  const [modalShow, setModalShow] = useState(false);
+
+  function modalClose() {
+    setModalShow(false);
+  }
+
+  function modalOpen() {
+    setModalShow(true);
+  }
+  // function modalClose() {
+  //   setModalShow(false);
+  // }
+
+  // function modalOpen() {
+  //   setModalShow(true);
+  // }
+
   return (
-    <div>
-      hello world
+    <div id="app">
+      <Header />
+      <Main modalOpen={modalOpen}/>
+      <Footer />
+      {modalShow && < Modal modalClose={modalClose}/> }
     </div>
   );
 }
---
+++ 10000hours/src/Components/main/Main.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import './Main.css'
+import { useState } from 'react'
+export default function Main(props) {
+
+    const [expert, setExpert] = useState('');
+    const [traningTime,setTraningTime] = useState(0);
+    const [dday, setDday] = useState(0);
+    const [showResult, setShowResult] = useState(false);
+
+    function ResultShow() {
+        setShowResult(true);
+    }
+
+    function ResultClose() {
+        setShowResult(false);
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefalut();
+        setDday(Math.ceil(10000 / traningTime));
+    }
+
+
+  return (
+    <main>
+        <h2 className="a11y-hidden">필요한 시간 알아보기</h2>
+        <form className="cont-input" onSubmit={handleSubmit}>
+            <p className="txt-wannabe">나는 
+                <input type="text" placeholder="예&#41프로그래밍" required value={expert} onChange={(event) => props.setExpert(event.target.value)}/> 
+                전문가가 될 것이다.
+            </p>
+            <p c lass="txt-time">그래서 앞으로 매일 하루에 
+                <input type="number" placeholder="예&#41 5시간" required value={traningTime} onChange={(event) => props.setTime(event.target.value)}/> 시간씩 훈련할 것이다.</p>
+            <button type="button" className="btn-exc">나는 며칠동안 훈련을 해야 1만시간이 될까?</button>
+        </form>
+
+        {((dday > 1) && (dday < 24)) && <section className="cont-section">}
+            <h3 className="a11y-hidden">결과 확인</h3>
+            <p className="txt-wannabe">
+                당신은 <strong>{expert}</strong> 전문가가 되기 위해서
+            </p>
+            <p className="txt-time">
+                대략 <strong>{dday}</strong> 일 이상 훈련하셔야 합니다! :&#41
+            </p>
+            <button className="btn-go" type="button" onClick={props.modalOpen}>훈련하러가기 GO!GO!</button>
+            <button className="btn-share" type="button">공유하기</button>
+        </section>
+    </main>
+  )
+}
